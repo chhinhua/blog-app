21aa92b615f820a316da659558d908bbc1f03044
chhinhua <chhinhua@gmail.com>
2023-03-30 22:19:17 +0700

    Define API information using Annotations

--- src/main/java/com/springboot/blog/SpringBootBlogRestApiApplication.java
+++ src/main/java/com/springboot/blog/SpringBootBlogRestApiApplication.java
@@ -1,11 +1,36 @@
 package com.springboot.blog;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Spring Boot Blog App REST API's",
+				description = "Spring Boot Blog App REST API's Documentation",
+				version = "v1.0",
+				contact = @Contact(
+						name = "Chhin Hua",
+						email = "chhinhua@gmail.com",
+						url = "https://www.linkedin.com/in/chhinhua/"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.javaguides.net/license"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Spring Boot Blog App Documentation",
+				url = "https://github.com/chhinhua/spring-boot-blog-rest-api"
+		)
+)
 public class SpringBootBlogRestApiApplication {
 
 	@Bean
--- src/main/java/com/springboot/blog/config/SecurityConfig.java
+++ src/main/java/com/springboot/blog/config/SecurityConfig.java
@@ -51,6 +51,8 @@ public class SecurityConfig {
                 .authorizeHttpRequests((authorize) -> authorize
                         .requestMatchers(HttpMethod.GET, "/api/**").permitAll()
                         .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/swagger-ui/**").permitAll()
+                        .requestMatchers("/v3/api-docs/**").permitAll()
                         .anyRequest()
                         .authenticated()
 
